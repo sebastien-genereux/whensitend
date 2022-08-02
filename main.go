aa3ea8e288e8f079184b04d8693dd1641cfea129
sebastien-genereux <sebastiengenereux12@gmail.com>
2022-08-01 22:43:54 -0400

    basic duration adding

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"time"
+	"strconv"
 )
 
 type pageConfig struct {
@@ -25,25 +26,37 @@ const timeFormat string = "02/01/2006"
 //configData saves the page's parsed config while making it available to the handlers
 var configData pageConfig
 
+func calcEnd(start time.Time, duration int) (endDate time.Time) {
+	return start.AddDate(0, 0, duration)
+}
+
 func endHandler(w http.ResponseWriter, r *http.Request) {
 	stringDate := fmt.Sprintf("%02s", r.FormValue("day")) + "/" + fmt.Sprintf("%02s", r.FormValue("month")) + "/" + r.FormValue("year")
 	log.Printf(stringDate)
 
 	startTime, err := time.Parse(timeFormat, stringDate)
 
-	if err != nil {
+	duration, err2 := strconv.Atoi(r.FormValue("duration"))
+
+	if err != nil && err2 != nil{
 		log.Printf(startTime.Format(timeFormat))
-		configData.Output = "Invalid date entered! Try again"
+		configData.Output = "Invalid date or length entered! Try again"
 	}else {
-		configData.Output = "dd/mm/yyyy:" + startTime.Format(timeFormat)
+		configData.Output = "End Date (dd/mm/yyyy): " + calcEnd(startTime, duration).Format(timeFormat)
 	}
 
     t, _ := template.ParseFiles("./web/templates/index.html")
 	t.Execute(w, configData) 
 }
 
+func homeHandler(w http.ResponseWriter, r *http.Request){
+	t, _ := template.ParseFiles("./web/templates/index.html")
+	t.Execute(w, nil) 
+}
+
 func main() {
 	// Serve the web application
+	http.HandleFunc("/", homeHandler)
     http.HandleFunc("/end", endHandler)
 
 	log.Printf("Starting server on 127.0.0.1:%s\n", serverPort)

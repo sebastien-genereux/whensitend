d4ff7af7d707c6e1f5ea1ef8913fd464af9fc05d
sebastien-genereux <sebastiengenereux12@gmail.com>
2022-08-02 22:32:48 -0400

    sample config and skipping day logic implemented

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # When does it end?
 
-A small web app that tells you when an activity ends, based on a start date and length. It will do this while excluding all those dates specified in the configuration
+A small web app that tells you when an activity ends, based on a start date and length. It will do this while excluding all those dates specified in the configuration.
+
+Specify days off for the activity in configs/conf.json
+
+Configuration parsing supports specific dates in dd/mm/yyyy format or any day of the week as per the golang time package weekday [type](https://pkg.go.dev/time#Weekday)
+
+Note the start and end date are inclusive!
--- main.go
+++ main.go
@@ -7,10 +7,18 @@ import (
 	"net/http"
 	"time"
 	"strconv"
+	"os"
+	"encoding/json"
 )
 
 type pageConfig struct {
 	Output		string
+}
+
+type appConfig struct {
+	DaysOfWeek []int `json:"daysOfWeek"`
+	SpecificDatesStr []string `json:"specificDates"`
+	specificDates []time.Time
 }
 
 /* SETTINGS */
@@ -21,13 +29,61 @@ const serverPort string = "8080"
 // output time format
 const timeFormat string = "02/01/2006"
 
+// configPath contains the relative or absolute path to the event configuration being used to parse event details
+const configPath string = "./configs/conf.json"
+
 /* GLOBALS */ 
 
-//configData saves the page's parsed config while making it available to the handlers
-var configData pageConfig
+// pageConf saves the page's parsed config while making it available to the handlers
+var pageConf pageConfig
+
+// appConf saves the applications parsed config while making it available to other functions
+var appConf appConfig
 
 func calcEnd(start time.Time, duration int) (endDate time.Time) {
-	return start.AddDate(0, 0, duration)
+	var daysToSkip bool = true
+	var oldEnd time.Time = start
+	var interval int = duration
+	var newEnd time.Time
+
+	for daysToSkip {
+		log.Print("new round")
+		newEnd = oldEnd.AddDate(0, 0, interval)
+
+		skips := 0
+		for d := oldEnd; d.After(newEnd) == false; d = d.AddDate(0, 0, 1) {
+			var alreadySkipped bool = false
+
+			for _, dayOff := range appConf.DaysOfWeek {
+				log.Printf("day off in conf %d", dayOff)
+				log.Printf("day checking %d", int(d.Weekday()))
+				if int(d.Weekday()) == dayOff{
+					skips += 1
+					alreadySkipped = true
+					break
+				}
+			}
+
+			if !alreadySkipped{
+				for _, dateOff := range appConf.specificDates {
+					if d == dateOff{
+						skips += 1
+						break
+					}
+				}
+			}
+		}
+
+		if skips != 0{
+			// shift the new interval checking for skip days to start after the current one (non-overlapping) as well as -1 from the skips to make the calc inclusive
+			interval = skips - 1
+			oldEnd = newEnd.AddDate(0, 0, 1)
+		}else{
+			daysToSkip = false
+		}
+	}
+
+	return newEnd
 }
 
 func endHandler(w http.ResponseWriter, r *http.Request) {
@@ -39,14 +95,14 @@ func endHandler(w http.ResponseWriter, r *http.Request) {
 	duration, err2 := strconv.Atoi(r.FormValue("duration"))
 
 	if err != nil && err2 != nil{
-		log.Printf(startTime.Format(timeFormat))
-		configData.Output = "Invalid date or length entered! Try again"
+		pageConf.Output = "Invalid date or length entered! Try again"
 	}else {
-		configData.Output = "End Date (dd/mm/yyyy): " + calcEnd(startTime, duration).Format(timeFormat)
+		// the minus one on duration is to make it an inclusive of start/end calculation
+		pageConf.Output = "End Date (dd/mm/yyyy): " + calcEnd(startTime, duration-1).Format(timeFormat)
 	}
 
     t, _ := template.ParseFiles("./web/templates/index.html")
-	t.Execute(w, configData) 
+	t.Execute(w, pageConf) 
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request){
@@ -54,7 +110,42 @@ func homeHandler(w http.ResponseWriter, r *http.Request){
 	t.Execute(w, nil) 
 }
 
+func parseAppConf(){
+	// Read the config file
+	configFile, err := os.ReadFile(configPath)
+	if err != nil {
+		log.Fatalln(err)
+	}	
+
+	// Parse the configuration file and unmarshal into global config variable
+	err = json.Unmarshal(configFile, &appConf)
+	if err != nil {
+		log.Fatal("Error during Unmarshal() of config ", err)
+	}
+
+	for _, day := range appConf.DaysOfWeek {
+		if day >= int(time.Sunday) && day <= int(time.Saturday) {
+			appConf.DaysOfWeek = append(appConf.DaysOfWeek, day)
+		} else {
+			log.Fatal("Invalid day of week found in config")
+		}
+	}
+
+	for _, dateStr := range appConf.SpecificDatesStr {
+		date, err := time.Parse(timeFormat, dateStr)
+
+		if err != nil{
+			log.Fatal("Invalid dd/mm/yyyy date found in config")
+		}
+
+		appConf.specificDates = append(appConf.specificDates, date)
+	}
+}
+
 func main() {
+	// Parse the configuration
+	parseAppConf()
+
 	// Serve the web application
 	http.HandleFunc("/", homeHandler)
     http.HandleFunc("/end", endHandler)
